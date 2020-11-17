89eac1294472bb88aff3a4a427652397d0263cce
Samson Akanbi <samson.akanbi@outlook.com>
2020-11-17 21:34:43 +0100

    Used a view engine for the view.

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
-
+const ejs = require('ejs')
+const path = require('path')
+const expressLayout = require('express-js-layouts')
 const PORT = process.env.PORT || 3000
 
 app.get('/', function(req, res) {
-    res.send('Hello from server')
+    res.render('Hello from server')
 })
+
+// Set template engine
+app.use(expressLayout)
+app.use('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
