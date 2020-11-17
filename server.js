179e1527a643b40d6a1e9311c0214be27e9401db
Samson Akanbi <samson.akanbi@outlook.com>
2020-11-17 22:08:35 +0100

    Created the express server.

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const path = require('path')
-const expressLayout = require('express-js-layouts')
+const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
 
-app.get('/', function(req, res) {
-    res.render('Hello from server')
+app.get('/', (req, res) => {
+    res.render('home')
 })
 
 // Set template engine
 app.use(expressLayout)
-app.use('views', path.join(__dirname, '/resources/views'))
+app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
 app.listen(PORT, () => {
