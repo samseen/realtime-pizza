e073bb2f9c392da967fa5f7a40cbcedf41c8aee5
Samson Akanbi <samson.akanbi@outlook.com>
2020-11-26 23:07:10 +0100

    Used cleaner routes.

--- routes/web.js
+++ routes/web.js
@@ -1,16 +1,12 @@
 const authController = require('../app/http/controllers/authController')
 const homeController = require ('../app/http/controllers/homeController')
+const cartController = require ('../app/http/controllers/customers/cartController')
 
 function initRoutes(app) {
     
     app.get('/', homeController().index)
-    // app.get('/', (req, res) => {
-    //     res.render('home')
-    // })
 
-    app.get('/cart', (req, res) => {
-        res.render('customers/cart')
-    })
+    app.get('/cart', cartController().index)
     
     app.get('/register', authController().register)
     
