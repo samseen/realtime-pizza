d618ad721ad2db2a59f853d0be229d67c7397a99
Samson Akanbi <samson.akanbi@outlook.com>
2020-11-29 11:28:53 +0100

    Printed the add button to screen upon clicking it.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-console.log("Hello from app js")
+let addToCart = document.querySelectorAll('.add-to-cart')
+
+addToCart.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        console.log(e)
+    })
+})
