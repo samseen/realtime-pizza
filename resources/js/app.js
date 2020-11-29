637fa8c24c986389a46025309b712cbca8d568a0
Samson Akanbi <samson.akanbi@outlook.com>
2020-11-29 11:36:57 +0100

    Installed axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,15 @@
+const { update } = require("../../app/models/menu")
+
 let addToCart = document.querySelectorAll('.add-to-cart')
+
+function updateCart(pizza) {
+    // 
+}
 
 addToCart.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-        console.log(e)
+        // let pizza = btn.dataset.pizza
+        let pizza = JSON.parse(btn.dataset.pizza)
+        updateCart(pizza)
     })
 })
