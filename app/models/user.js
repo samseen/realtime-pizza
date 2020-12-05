83c990427db8085cbee3a6bf75e4f637792a6dad
Samson Akanbi <samson.akanbi@outlook.com>
2020-12-05 21:44:46 +0100

    Completed the user schema.

--- app/models/user.js
+++ app/models/user.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const menuSchema = new Schema({
+const userSchema = new Schema({
     name: { type: String, required: true },
-    image: { type: String, required: true },
-    price: { type: Number, required: true },
-    size: { type: String, required: true }
-})
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, default: 'customer' }
+}, { timestamps: true })
 
-module.exports = mongoose.model('Menu', menuSchema)
+module.exports = mongoose.model('User', userSchema)
